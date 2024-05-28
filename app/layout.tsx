e1d79a7cd0f6feb07ede9f0609765519499c6213
Sovannarith.Chin <125848522+sovanarith-8888@users.noreply.github.com>
2024-05-28 15:15:37 +0700

    feat(icon-tabbar): Change default icon tabbar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="favicon.svg" sizes="32x32" type="image/x-icon" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );

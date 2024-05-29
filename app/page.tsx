724f39ffd2a77d283f2a0b2d22f539350a482bdf
Sovannarith.Chin <125848522+sovanarith-8888@users.noreply.github.com>
2024-05-29 11:27:02 +0700

    fix(conflict merge): clear text conflict merge.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
 'use client';
 import Image from "next/image";
 import  { svgRepo }  from "@/components/icons/svgrepo";
-<<<<<<< HEAD
+
 import {Button} from '@nextui-org/button'; 
-=======
 import {uiBall} from '../components/animation/uiball';
 
->>>>>>> animation
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<<<<<<< HEAD
       <Button>Click me as UIBall</Button>
-=======
-
->>>>>>> animation
     </main>
   );
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import {NextUIProvider} from '@nextui-org/react';
-<<<<<<< HEAD
-=======
 import React from 'react';
->>>>>>> animation
+
 
 export function Providers({children}: {children: React.ReactNode}){
   return (
